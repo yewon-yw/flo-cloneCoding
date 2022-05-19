46bd1e391aeea6143d37ce02ce48241383653d5f
yewon-yw <rladnjs0401@naver.com>
2022-05-20 01:55:28 +0900

    week9

---
+++ app/src/main/java/com/example/practice/LookView.kt
@@ -0,0 +1,7 @@
+package com.example.practice
+
+interface LookView {
+    fun onGetSongLoading()
+    fun onGetSongSuccess(code: Int, result: FloChartResult)
+    fun onGetSongFailure(code: Int, message: String)
+}
